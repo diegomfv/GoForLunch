888caa7e483c0c1faa82d62e98bc959e3d608202
diegoma77 <31185512+diegoma77@users.noreply.github.com>
2018-05-07 01:52:40 +0100

    Create Preferences Layout

--- app/src/main/java/com/example/android/goforlunch/pageFragments/SettingsFragment.java
+++ app/src/main/java/com/example/android/goforlunch/pageFragments/SettingsFragment.java
@@ -1,8 +1,20 @@
 package com.example.android.goforlunch.pageFragments;
 
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import com.example.android.goforlunch.R;
+
 /**
  * Created by Diego Fajardo on 07/05/2018.
  */
 
-public class SettingsFragment extends  {
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_go_for_lunch);
+
+    }
 }
